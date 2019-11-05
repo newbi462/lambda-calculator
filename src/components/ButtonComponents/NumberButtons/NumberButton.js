38eec6fb9cc39336337ea04cc4dccea41b5ec4da
newbi462 <isthisemailfree@gmail.com>
2019-11-05 15:04:29 -0500

    Get pushing

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-const NumberButton = () => {
+import { numbers } from '../../../data';
+import { Numbers } from './Numbers';
+
+export const NumberButton = (  {portToButtonsPropPass} ) => {
+
+  let passToMap = {
+    numbers: numbers,
+    state: portToButtonsPropPass
+  }
+
+
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down
+        from the parent container on props */}
+        <>
+          <Numbers anyVarNameForProp={passToMap} />
+        </>
     </>
   );
 };
