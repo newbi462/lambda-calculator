e33afa109ca4a93c02fde547e6b055528152de3e
newbi462 <isthisemailfree@gmail.com>
2019-11-05 15:17:31 -0500

    Buttons and data

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -4,13 +4,17 @@ import React from "react";
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+
+export const Operators = (  {propPass}  ) => {
   // STEP 2 - add the imported data to state
   return (
-    <div>
+    <>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-    </div>
+       {propPass.map(function(item) {
+         return <button>{item.char}</button>;
+       })}
+    </>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const SpecialButton = () => {
+import { specials } from '../../../data';
+import { Specials } from './Specials';
+
+export const SpecialButton = () => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <Specials propPass={specials} />
     </>
   );
 };
